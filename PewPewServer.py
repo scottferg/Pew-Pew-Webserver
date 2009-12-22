0ec79e39b5b662cd5d9b1791b1bc4dc5adc44e4c
Scott Ferguson <sferguson@quikorder.com>
2009-12-22 15:31:33 -0600

    Develop app framework based on GSD

--- PewPewServer.py
+++ PewPewServer.py
@@ -9,9 +9,10 @@ import fileParser
 
 class RequestHandler(BaseHTTPRequestHandler):
     RENDERABLE_FILETYPES = ['htm', 'html', 'php']
+    BINARY_FILETYPES = ['jpg', 'jpeg', 'gif', 'png']
 
-    def __init__(self, name, *args, **kwargs):
-        self.name = name
+    def __init__(self, app, *args, **kwargs):
+        self.app = app
         self.setup_mime_types()
         BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
         
@@ -23,6 +24,16 @@ class RequestHandler(BaseHTTPRequestHandler):
             if requestType == 'XMLHttpRequest':
                 url = urlparse(self.path)
                 params = dict([part.split('=') for part in url[4].split('&')])
+
+                requestPath = url[2].replace('/', '_').replace('.', '_')
+
+                try:
+                    get_request = getattr(self.app, 'GET%s' % requestPath)
+                except AttributeError, e:
+                    self.send_error(404, 'Error: %s' % e)
+                else:
+                    get_request(params)
+
             elif self.path:
                 file = (self.path == '/') and 'index.php' or self.path[1:]
                 
@@ -46,19 +57,20 @@ class RequestHandler(BaseHTTPRequestHandler):
             # Remove query string parameters
             parsedFilename = urlparse(filename)[2]
 
-            self.send_headers(value = mimetypes.guess_type(parsedFilename)[0], 
-                              length = str(os.path.getsize(parsedFilename)))
-            self.wfile.write(open(parsedFilename.replace('/', os.sep)).read())
+            mode = 'r'
+
+            if extension in self.BINARY_FILETYPES:
+                mode = 'rb'
+
+            self.send_headers(value = mimetypes.guess_type(parsedFilename)[0])
+            self.wfile.write(open(parsedFilename.replace('/', os.sep), mode).read())
 
         return (extension in self.RENDERABLE_FILETYPES) and render_page or send_static_file
 
-    def send_headers(self, response = 200, key = 'Content-type', value = 'text/html', length = False):
+    def send_headers(self, response = 200, key = 'Content-type', value = 'text/html'):
         self.send_response(response)
+        self.send_header('Transfer-Encoding', 'chunked')
         self.send_header(key, value)
-        
-        if length:
-            self.send_header('Content-length', length)
-
         self.end_headers()
 
     def setup_mime_types(self):
@@ -66,13 +78,13 @@ class RequestHandler(BaseHTTPRequestHandler):
         mimetypes.add_type('application/x-javascript', '.js')
         mimetypes.add_type('image/png', '.png')
 
-class App:
+class App(object):
     def __init__(self, name, port):
         self.name = name
         self.port = port
 
     def Handler(self, *args, **kwargs):
-        return RequestHandler(self.name, *args, **kwargs)
+        return RequestHandler(self, *args, **kwargs)
 
     def start(self):
         try:
